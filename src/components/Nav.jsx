816514d5c56ef2853e83009cf20f8d0b1c7f8431
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-08-22 21:27:20 +0700

    setup project using tailwind

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
     return (
-        <div className="container flex justify-center items-center">
-            <ul className='flex justify-center items-center w-full bg-slate-700 h-12'>
+        <div className="container flex justifer itemy-center items-center">
+            <ul className='flex justify-cents-center w-full bg-slate-700 h-12'>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/project">Project</Link></li>
