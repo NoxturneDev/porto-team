0744c8d276d0f8fb48b1b3b917155d13944cd202
IkoRaga <ikoraga@gmail.com>
2022-08-23 13:23:17 +0700

    initial create navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <div className="container flex justifer itemy-center items-center">
-            <ul className='flex justify-cents-center w-full bg-slate-700 h-12'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/project">Project</Link></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="flex justify-center items-center w-screen bg-slate-700 h-16">
+      <div className="grow p-4 text-gray-50 text-2xl font-bold">
+        <h2>HALLOWEB</h2>
+      </div>
+      <ul className="flex justify-center items-center color-white h-14 mx-10">
+        <li className="m-8 text-gray-50">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="m-8  text-gray-50">
+          <Link to="/about">About Us</Link>
+        </li>
+        <li className="m-8  text-gray-50 ">
+          <Link to="/project">Projects</Link>
+        </li>
+        <li className="m-8  text-gray-50">
+          <Link to="/project">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from "react";
+import Nav from "../components/Nav";
 function Project() {
   return (
     <>
       <Nav />
-      <h3 className='text-teal-400'>PROJECT</h3>
     </>
-  )
+  );
 }
 
-export default Project
+export default Project;
