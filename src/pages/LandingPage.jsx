2d683cc2f84df7b5c22d563d8a5b279b452c953a
IkoRaga <ikoraga@gmail.com>
2022-08-23 13:56:39 +0700

    create basic card sm md lg

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import { Container } from '../components/Container'
-import Button from '../components/ui/Button'
+import React from "react";
+import Nav from "../components/Nav";
+import { Container } from "../components/Container";
+import Button from "../components/ui/Button";
 
 function LandingPage() {
   return (
     <>
       <Nav />
       <Container custom="bg-yellow-400" full={true} type="col-center">
-          <h3 className='font-bold text-red-400'>JUMBOTRON</h3>
-          <Button animate={true}>Test</Button>
+        <h3 className="font-bold text-red-400">JUMBOTRON</h3>
+        <Button animate={true}>Test</Button>
+        <div className="card-lg"></div>
       </Container>
-
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
