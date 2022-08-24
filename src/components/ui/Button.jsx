ac1a97e4a91138b054f36735e91bde95d2fc4643
IkoRaga <ikoraga@gmail.com>
2022-08-24 13:35:05 +0700

    test container

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 const buttonType = {
-    fill: {
-        primary: 'button-primary',
-        secondary: 'button-secondary'
-    },
-    line: {
-        primary: 'button-primary-line',
-        secondary: 'button-secondary-line'
+  fill: {
+    primary: "button-primary",
+    secondary: "button-secondary",
+  },
+  line: {
+    primary: "button-primary-line",
+    secondary: "button-secondary-line",
+  },
+};
+//type : primary, secondary
+// fill : true, false
+function Button({
+  children,
+  custom,
+  type = buttonType.primary.fill,
+  animate,
+  fill = true /*boolean */,
+}) {
+  function handleType(type) {
+    if (!fill) {
+      for (const button in buttonType.line) {
+        if (type === button) return buttonType.line[button];
+      }
+    } else {
+      for (const button in buttonType.fill) {
+        if (type === button) return buttonType.fill[button];
+      }
     }
-}
+  }
 
-function Button({ children, custom, type = buttonType.primary.fill, animate, fill = true /*boolean */ }) {
-
-    function handleType(type) {
-        if (!fill) {
-            for (const button in buttonType.line) {
-                if (type === button) return buttonType.line[button]
-            }
-        } else {
-            for (const button in buttonType.fill) {
-                if (type === button) return buttonType.fill[button]
-            }
-        }
-    }
-
-    // if animate is false, return without motion
-    if (!animate) {
-        return <button className={`${handleType(type)} ${custom}`}>{children}</button>
-    }
-
+  // if animate is false, return without motion
+  if (!animate) {
     return (
-        <motion.button
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ type: 'tween' }}
-            className={`${handleType(type)} ${custom}`}>{children}</motion.button>
-    )
+      <button className={`${handleType(type)} ${custom}`}>{children}</button>
+    );
+  }
+
+  return (
+    <motion.button
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ type: "tween" }}
+      className={`${handleType(type)} ${custom}`}
+    >
+      {children}
+    </motion.button>
+  );
 }
 
-export default Button
+export default Button;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,9 +1,41 @@
+import { Container } from "../components/Container";
 import React from "react";
 import Nav from "../components/Nav";
+import { Link } from "react-router-dom";
+
 function Project() {
   return (
     <>
       <Nav />
+      <Container custom="" full={true} type="col-top">
+        <div className="" full={true}>
+          <div className="font-bold text-4xl tracking-wider text-center">
+            <h1>PROJECTKU</h1>
+          </div>
+          <Container custom="bg-red-400" type="left" full={true}>
+            <Container
+              custom="bg-yellow-200 shadow-2xl h-3/4"
+              type="center"
+            ></Container>
+            <Container custom="" type=" col-top">
+              <Container className="font-bold">
+                <h2>LOREM ANJAY SLEBEW</h2>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur, magni.
+                </p>
+                <Container>
+                  <Link to="/">View Project</Link>
+                </Container>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Enim, suscipit.
+                </p>
+              </Container>
+            </Container>
+          </Container>
+        </div>
+      </Container>
     </>
   );
 }
