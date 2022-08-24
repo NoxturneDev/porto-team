a9d432a8379f2e72015b893bab035fc88d0aae3d
IkoRaga <ikoraga@gmail.com>
2022-08-24 14:13:02 +0700

    create new project component

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,27 +7,32 @@ function Project() {
   return (
     <>
       <Nav />
-      <Container custom="" full={true} type="col-top">
+      <Container custom="" full={true} dir="column" align="top">
         <div className="" full={true}>
-          <div className="font-bold text-4xl tracking-wider text-center">
-            <h1>PROJECTKU</h1>
+          <div className="font-bold text-4xl tracking-wider text-center ">
+            <h1 className="tracking-[.25em]">PROJECTKU</h1>
           </div>
-          <Container custom="bg-red-400" type="left" full={true}>
+          <Container custom="pl-60" align="left" full={true}>
+            <Container custom=" shadow-2xl h-3/4 mr-4"></Container>
             <Container
-              custom="bg-yellow-200 shadow-2xl h-3/4"
-              type="center"
-            ></Container>
-            <Container custom="" type=" col-top">
-              <Container className="font-bold">
-                <h2>LOREM ANJAY SLEBEW</h2>
-                <p>
+              custom="w-100 h-3/4 translate-x-[-17rem] bg-slate-500"
+              dir="column"
+            >
+              <Container custom="h-4/4" dir="column" align="left">
+                <h2 className="font-bold text-4xl text-yellow-50 ">
+                  LOREM ANJAY SLEBEW
+                </h2>
+                <p className="font-medium text-2xl my-2 text-yellow-100">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                   Consequuntur, magni.
                 </p>
-                <Container>
+                <Container
+                  align="left"
+                  custom="font-bold text-3xl text-yellow-100"
+                >
                   <Link to="/">View Project</Link>
                 </Container>
-                <p>
+                <p className="font-medium text-lg text-yellow-500">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Enim, suscipit.
                 </p>
