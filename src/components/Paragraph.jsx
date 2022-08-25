9fb538ef5e5076000348ad276a172a74ff397bb3
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-08-25 10:57:15 +0700

    new paragraph components

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+/**
+ * 
+ * @param {String} children - component's children
+ * @param {String} custom - custom tailwind classes for the components
+ * @param {boolean} animate - default : false - animation state, return framer motion components if true
+ * @returns react components
+ */
 
-function Paragraph({children}) {
+function Paragraph({ children, custom = '', animate = false }) {
+  if(animate){
+    <motion.p
+    initial={{opacity: 0, y: -25}}
+    animate={{opacity: 1, y: 0}}
+    transition={{type: "spring", duration: 1}}
+    className={`paragraph ${custom}`}
+    >
+
+    </motion.p>
+  }
   return (
-    <p className=''>{children}</p>
+    <p className={`paragraph ${custom}`}>{children}</p>
   )
 }
 
