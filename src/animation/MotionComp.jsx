547151a90644e31b03c10346e2899ab60dac085c
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-08-28 18:07:24 +0700

    new variants framer motion obj init

--- src/animation/MotionComp.jsx
+++ src/animation/MotionComp.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import animations from './variants.js'
 
 /**
  * 
- * @param {Object} variants - variants animation object for framer motion
+ * @param {String} animation - animation type for the components. values "{animation name}-{direction}", ex : 'fade-left'
  * @param {Object} children - component's children 
  * @returns framer motion components
  */
 
-function MotionComp({ children, variants}) {
+const variantList = {
+    'fade-left': animations.fade.left,
+    'fade-right': animations.fade.right,
+    'fade-up': animations.fade.up,
+    'fade-bottom': animations.fade.bottom,
+    'slide-left': animations.slide.left,
+    'slide-right': animations.slide.right,
+    'slide-up': animations.slide.up,
+    'slide-down': animations.slide.bottom,
+}
+
+function MotionComp({ children, animation, transition }) {
+
+    const variants = () => {
+        for (const variant in variantList) {
+            if (animation === variant) {
+                const obj = variantList[animation]
+
+                if (transition) {
+                    obj.visible.transition = transition
+                }
+
+                console.log(obj )
+                return obj
+            }
+        }
+    }
+
     return (
         <AnimatePresence exitBeforeEnter mode='wait'>
             <motion.div
-                variants={variants}
+                variants={variants()}
                 exit="exit"
                 animate="visible"
                 initial="hidden"
--- src/animation/variants.js
+++ src/animation/variants.js
@@ -15,29 +15,60 @@ const transitions = {
             mass: 0.5,
             stiffness: 50
         }
+    },
+    tween: {
+        normal: {
+            type: "tween",
+            duration: 1
+        },
+        slow: {
+            type: "tween",
+            duration: 2
+        },
+        fast: {
+            type: "tween",
+            duration: 0.5
+        }
     }
 }
 
 
-function initVariants(hidden, visible, exit, transition = transitions.spring.normal, time = {}) {
-    return {
-        hidden: hidden,
+function initVariants(obj) {
+    const { init, start, exit, transition, time } = obj
+
+    const variants = {
+        hidden: init,
         visible: {
-            ...visible,
+            ...start,
             transition: { ...transition, ...time }
         },
         exit: exit
     }
+
+    return variants
 }
 
 // FRAMER MOTION VARIANTS
 const fade = {
-    left: initVariants(
-        { opacity: 0, x: '-100%' }, { opacity: 1, x: 0 }, { opacity: 0, x: '-100%' }
-    ),
-    right: initVariants(
-        { opacity: 0, x: '100%' }, { opacity: 1, x: 0 }, { opacity: 0, x: '100%' }
-    ),
+    left: initVariants({
+        init: { opacity: 0, x: '-100%' },
+        start: { opacity: 1, x: 0 },
+        exit: { opacity: 0, x: '-100%' },
+        transition: { ...transitions.spring.normal },
+        time: {
+            duration: 1,
+            delay: 2
+        }
+    }),
+    right: {
+        hidden: { opacity: 0, x: '100%' },
+        visible: {
+            opacity: 1,
+            x: '0',
+            transition: { ...transitions.spring.normal }
+        },
+        exit: { opacity: 0, x: '100%' }
+    },
     up: initVariants(
         { opacity: 0, y: '-100%' }, { opacity: 1, y: 0 }, { opacity: 0, y: '-100%' }
     ),
@@ -68,4 +99,4 @@ const animations = {
 
 export default animations
 
-// TODO : DYNAMIC TRANSITION, SMOOTHER ANIMATION, CUSTOM ANIMATION FUNCTION
+// TODO : DYNAMIC TRANSITION, SMOOTHER ANIMATION, CUSTOM ANIMATION FUNCTION, GESTURES
