dbd3faa7037497f1bfea8659585a15e184779bf9
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-08-29 20:57:02 +0700

    new section reusable components

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Container } from './Container'
 
-function Section({ children, custom }) {
+function Section({ children, custom, align = "center"}) {
     return (
-        <Container  full={true} dir="column" align="top" custom={`${custom} p-0`}>
-            <Container align="left" full={true} custom="p-0">
-                {children}
-            </Container>
+        <Container full={true} align={align} custom={`${custom}`}>
+            {children}
         </Container>
     )
 }
