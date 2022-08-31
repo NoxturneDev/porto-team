93ebbb8ee4710bddc821bda801df31610f5cf5e3
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-08-31 17:37:20 +0700

    fix issue on project desc get overflowed by project img

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -46,16 +46,17 @@ function ProjectSectionRight({ title = "TITLE", img, url, desc, animation }) {
     if (animation) {
         return (
             <Section custom='bg-blue-400'>
-                <Container custom="h-3/4 translate-x-[8rem]">
+                <Container custom="w-100 h-3/4 translate-x-[8rem] z-10">
                     <MotionComp animation="fade-left">
                         <ProjectDesc title={title} url={url} desc={desc} align="right" />
                     </MotionComp>
                 </Container>
                 <Container custom="shadow-2xl p-0 h-max mr-10">
-                    <Image url={img} custom="w-full h-auto" rounded={true} animate="true" />
+                    <MotionComp animation="fade-up">
+                        <Image url={img} custom="w-full h-auto" rounded={true}  />
+                    </MotionComp>
                 </Container>
             </Section >
-
         )
     }
 
