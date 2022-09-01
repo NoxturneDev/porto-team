364b1b9d42cc6ca3b3d0b76812122e22ead96491
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-01 16:26:34 +0700

    adding new components to each pages, and tutotiral on landing pages

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,30 +6,35 @@ import Card from "../components/ui/Cards"
 import Image from "../components/ui/Image";
 import Caraousel from "../components/caraousel/Caraousel";
 import Section from "../components/Section";
-import MotionComp from "../animation/MotionComp";
+import MotionComp from "../animation/Motion";
+
 
 
 function LandingPage() {
   return (
     <div style={{ overflowX: 'hidden' }}>
       <Nav />
-      <Container custom="bg-slate-500 h-screen" align="left" full={true} dir='column'>
-        <Image url="../assets/23.png" type="circle" custom="bg-slate-800" size="sm" animate="true"></Image>
-        <Image url="../assets/23.png" type="basic" custom="bg-slate-800" size="sm"></Image>
-        <Image url="../assets/23.png" type="basic" custom="bg-slate-800" size="sm"></Image>
-        <Image url="../assets/23.png" type="circle" custom="bg-slate-800" size="sm"></Image>
-      </Container>
-      <Container dir="column" align="left">
-        <Card></Card>
-        <Card></Card>
-      </Container>
       <Caraousel></Caraousel>
-      <Section></Section>
-      <MotionComp animation="fade-up" gestures="hover" gesturesAnimation="scale-in">
-        <Container custom="bg-slate-400" full="true">
-          <Image url="../assets/23.png" type="circle" custom="bg-slate-800" size="sm"></Image>
-        </Container>
+      <MotionComp animation="fade-left" transition={{ type: 'tween', speed: 'fast' }}>
+        <Container full custom="bg-red-500"></Container>
       </MotionComp>
+      <Container full custom="bg-blue-500">
+        <Button type="primary" animation="fade-left">TEST</Button>
+        <Button type="primary" animation="fade-left">TEST</Button>
+        <Button type="primary" animation="fade-left">TEST</Button>
+        <Button type="primary" animation="fade-left">TEST</Button>
+      </Container>
+      <Container full custom="bg-blue-400">
+        <Card size="sm" animation="fade-left">TEST</Card>
+        <Card size="md" animation="fade-up">TEST</Card>
+        <Card size="md" animation="fade-bottom">TEST</Card>
+        <Card size="lg" animation="fade-right">TEST</Card>
+      </Container>
+      <Container full custom="bg-blue-600" dir="column">
+          <Image url="../assets/23.png" size="md" type="circle" />
+          <Image url="../assets/23.png" size="sm" type="rounded" />
+          <Image url="../assets/23.png" size="md" type="basic" />
+      </Container>
     </div>
   );
 }
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../components/Nav";
-import ProjectSection from "../components/project/ProjectSection";
+import { ProjectSectionLeft, ProjectSectionRight } from "../components/project/ProjectSection";
+
 function Project() {
   const description = ['lorem anjay mabar mantpa jiwa aiowdnaownd', 'aidnai aijn anjir keren banget ini gwa cokkks']
   return (
@@ -9,10 +10,10 @@ function Project() {
       <div className="font-bold text-center ">
         <h1 className="tracking-[.25em] header-lg">PROJECTKU</h1>
       </div>
-      <ProjectSection title="TITLE 1" url="/" img="../assets/23.png" desc={description} />
-      <ProjectSection title="TITLE 2" url="/" img="../assets/23.png" desc={description} />
-      <ProjectSection title="TITLE 3" url="/" img="../assets/23.png" desc={description} />
-      <ProjectSection title="TITLE 4" url="/" img="../assets/23.png" desc={description} />
+      <ProjectSectionLeft title="TITLE 1" url="/" img="../assets/23.png" desc={description} animation="true" />
+      <ProjectSectionRight title="TITLE 2" url="/" img="../assets/23.png" desc={description} animation="true" />
+      <ProjectSectionLeft title="TITLE 3" url="/" img="../assets/23.png" desc={description} />
+      <ProjectSectionRight title="TITLE 4" url="/" img="../assets/23.png" desc={description} />
     </>
   );
 }
