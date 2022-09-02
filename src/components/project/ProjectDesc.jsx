dcd737aa9acd98a7610a4a0a78f7c18507aca9d3
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-02 14:18:30 +0700

    change container component to wrapper component, prevent from adding padding

--- src/components/project/ProjectDesc.jsx
+++ src/components/project/ProjectDesc.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Container } from "../Container";
+import {  Wrapper } from "../Container";
 import Paragraph from "../Paragraph";
-import { Link } from "react-router-dom";
 
 function ProjectDesc({ title, url, desc, align = "left" }) {
   return (
-    <Container custom="h-4/4 text-slate-200" dir="column" align={align}>
+    <Wrapper custom="h-4/4 text-slate-200" dir="column" align={align}>
       <h2 className="header-sm text-slate-200">{title}</h2>
       <div className="my-5 ">
         <Paragraph custom="font-bold tracking-widest" align={align}>
@@ -13,12 +12,11 @@ function ProjectDesc({ title, url, desc, align = "left" }) {
         </Paragraph>
       </div>
       <div className="my-5 text-xl font-semibold tracking-widest underline">
-        {/* <Link to={url}>View Project</Link> */}
         <a href={url} target="blank">
           VIEW PROJECT
         </a>
       </div>
-    </Container>
+    </Wrapper>
   );
 }
 
