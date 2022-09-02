241b2a6f61aaeddd620414aca434163693cb1dd3
IkoRaga <ikoraga@gmail.com>
2022-09-02 19:08:42 +0700

    adding contact form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './css/index.css';
-import About from './pages/About'
-import LandingPage from './pages/LandingPage'
-import Project from './pages/Project'
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import "./css/index.css";
+import About from "./pages/About";
+import LandingPage from "./pages/LandingPage";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/project' element={<Project />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+function Contact() {
+  return (
+    <>
+      <Nav />
+      <div
+        className="h-screen flex flex-col 
+      items-center justify-center bg-slate-400 "
+      >
+        <form
+          className=" shadow-md rounded px-8 pt-6 pb-8 mb-4 h-3/4 w-2/4 bg-gray-200 flex border"
+          method="POST"
+          target="_blank"
+        >
+          <div className=" h-full w-2/4 align-items-center flex  flex-col">
+            <div className="mb-3 pt-0 mt-12">
+              <h1 className="text-4xl w-3/4 font-bold">
+                Fill the form. <br /> It's easy.
+              </h1>
+              <input
+                type="text"
+                placeholder="Your name"
+                name="name"
+                className="px-3 py-3 placeholder-gray-400 text-gray-600 relative mt-12 bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <textarea
+                placeholder="Your message"
+                name="message"
+                className="px-3 py-3 h-40 placeholder-gray-400 text-gray-600 relative  bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <button
+                className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="submit"
+              >
+                Send a message
+              </button>
+            </div>
+          </div>
+          <div className="inset-y-0 left-0 w-10 border flex items-center justify-center">
+            <div className="inset-y-0 left-0 w-1 h-3/4 bg-slate-300 rounded-lg"></div>
+          </div>
+          <div className="justify-center  h-full w-2/4 p-2 mt-12">
+            <h1 className="text-3xl font-bold mt-32 ">
+              Let's talk about <br /> everything.
+            </h1>
+            <p className="py-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+              laborum praesentium totam quibusdam beatae dignissimos fuga in cum
+              recusandae magnam?
+            </p>
+            <a href="#" className="">
+              <p className="text-red-500 py-5">Read More</p>
+            </a>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
