80780494976b1debb4aba7e072ba92ce6dbffc09
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-02 14:19:17 +0700

    fix height on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { Container } from "./Container";
+import { Wrapper } from "./Container";
 import Paragraph from "./Paragraph";
 
 const Footer = () => {
-
-
-    return(
+    return (
         <>
-           <Container custom='border-t-2 border-zinc-200   w-full h-40
-            bg-gray-900' align='center' >
-              <Paragraph align='center' custom='text-zinc-200 font-bold' >
-              Lorem Lorem ipsum dolor sit amet,
-              <br/>
-       consectetur adipiscing elit,
-              </Paragraph>
-           </Container>
+            <Wrapper custom='border-zinc-200 h-36 bg-gray-900' align='center' >
+                <Paragraph align='center' custom='text-zinc-200 font-bold' >
+                    Lorem Lorem ipsum dolor sit amet,
+                    <br />
+                    consectetur adipiscing elit,
+                </Paragraph>
+            </Wrapper>
         </>
     )
 }
