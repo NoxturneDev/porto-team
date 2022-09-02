3bd2235319c69722f8769d80f1c3cfb79cd8f48a
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-02 14:19:40 +0700

    deletin unsude code and refactor

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,37 +9,20 @@ import Footer from '../components/Footer';
 
 function About() {
   return (
-    <div style={{overflowX: 'hidden'}}>
+    <div style={{ overflowX: 'hidden' }}>
       <Nav />
       {/* <div className='flex bg-gray-900 items-center w-screen h-screen'>
           <h3 className='' >About Page</h3>
           <Container custom='bg-gray-900' >
           </Container>
         </div> */}
-      <Container custom='bg-gray-900 w-full' dir='column' full>
-
+      <Container custom='bg-gray-900 w-full h-[200vh]' dir='column' full>
         <h2 className='text-zinc-200 mb-5 font-bold text-4xl' >About Us</h2>
-        <div className='bg-slate-700 mt-7 mb-28   w-4/6 h-3/5'>
+        <div className='bg-slate-700 mt-7 mb-28   w-4/6 h-2/5'>
         </div>
-        <Paragraph custom='text-zinc-200 mt-28 pt-9 text-2xl font-bold' align='center'>Lorem Lorem ipsum dolor sit amet,
-          <br />
-          consectetur adipiscing elit</Paragraph>
-
-
-      </Container>
-
-      <Container custom='bg-gray-900 w-full' full>
-        {/* For galih carousel  */}
-
-        {/* <div  className='bg-slate-700 mr-7    w-4/6 h-3/5'>
-           </div>
-
-           <div  className='bg-slate-700 mr-7    w-4/6 h-3/5'>
-           </div>
-
-           <div  className='bg-slate-700 mr-7    w-4/6 h-3/5'>
-           </div> */}
-
+        <Paragraph custom='text-zinc-200 mt-28 pt-9 text-2xl font-bold' align='center'>
+          Lorem Lorem ipsum dolor sit amet <br /> consectetur adipiscing elit
+        </Paragraph>
       </Container>
 
       <Carousel />
