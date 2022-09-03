3ca1c62dbc9183afc1b3aab72c4feb048ab3b5d3
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-03 21:45:41 +0700

    add new font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      fontFamily:{
-        'poppins': ['Poppins', 'sans-serif']
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'clash': ['Clash Grotesk', 'sans-serif'],
+        'cabinet': ['Clash Grotesk', 'sans-serif'],
+        'satoshi': ['Satoshi', 'sans-serif'],
+        'generalsans': ['General Sans', 'sans - serif'],
       },
       colors: {
-        'dark' : '#1B2430',
+        'dark': '#1B2430',
         'semi-dark': '#38486A',
-        'light' : '#E7E1E1',
+        'light': '#E7E1E1',
         'semi-light': '#D9D9D9'
       }
     },
