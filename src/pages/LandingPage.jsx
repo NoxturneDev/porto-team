0bd37d78e5a32d22b775fd6dfc906c765b26f8d0
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-04 18:48:23 +0700

    clearing landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -16,46 +16,6 @@ return (
   <Nav />
   <JumbotronAnim />
   <Caraousel></Caraousel>
-  <MotionComp animation="fade-left" transition={{ type: 'tween', speed: 'fast' }}>
-    <Container full custom="bg-hazel-brown-200"></Container>
-  </MotionComp>
-  <Container full custom="bg-hazel-brown-200">
-    <Button type="primary" animation="fade-left">TEST</Button>
-    <Button type="primary" animation="fade-left">TEST</Button>
-    <Button type="primary" animation="fade-left">TEST</Button>
-    <Button type="primary" animation="fade-left">TEST</Button>
-  </Container>
-  <Container full custom="bg-blue-400">
-    <Card size="lg" custom='bg-nature-blue-100' animation="fade-left">
-      <Button custom='bg-astro-dark-100'>Astro Dark</Button>
-      <Button custom='bg-astro-dark-200'>Astro Dark</Button>
-    </Card>
-    <Card size="lg" custom='bg-astro-dark-200' animation="fade-up">
-    <Button custom='bg-astro-purple-100'>Astro Purple</Button>
-      <Button custom='bg-astro-purple-200'>Astro Purple</Button>
-      <Button custom='bg-astro-purple-300'>Astro Purple</Button>
-      
-    </Card>
-    <Card size="lg" custom='bg-astro-purple-300' animation="fade-bottom">
-    <Button custom='bg-hazel-blue-100'>Hazel Blue</Button>
-      <Button custom='bg-hazel-blue-200'>Hazel Blue</Button>
-      <Button custom='bg-hazel-blue-300'>Hazel Blue</Button>
-    </Card>
-    <Card size="lg" custom='bg-nature-green-200' animation="fade-right">
-    <Button custom='bg-nature-blue-200'>Natural Blue</Button>
-      <Button custom='bg-nature-blue-100'>Natural Blue</Button>
-    </Card>
-
-    <Card size="lg" custom='bg-nature-yellow' animation="fade-right">
-      <Button custom='bg-nature-green-100'>Nature Green</Button>
-    <Button custom='bg-nature-green-200'>Nature Green</Button>
-    </Card>
-  </Container>
-  <Container full custom="bg-blue-600" dir="column">
-    <Image url="../assets/23.png" size="md" type="circle" />
-    <Image url="../assets/23.png" size="sm" type="rounded" />
-    <Image url="../assets/23.png" size="md" type="basic" />
-  </Container>
   <Footer />
 </div>
 );
