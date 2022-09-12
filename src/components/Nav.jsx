7365fb9ed016715f0eb147c51d8300cd2907156b
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-12 12:48:05 +0700

    hover detection to change nav title

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,12 @@ import Sphere from "./Sphere";
 import { Toggle } from "./ui/Icon";
 
 function Nav() {
+  const [title, setTitle] = useState('Home')
+
+  const changeTitle = (e) => {
+    console.log(e.target)
+    setTitle(e.target.innerText)
+  }
   return (
     <div style={{
       background: 'radial-gradient(circle, #DB0F97, #C20C86)'
@@ -15,22 +21,26 @@ function Nav() {
       <Wrapper custom="px-10">
         <Toggle w="22px" h="22px" />
         <ul className="flex justify-center items-center color-white h-14 font-semibold text-dark-300 uppercase grow">
-          <li className="m-8 font-satoshi tracking-widest duration-200 transition">
+          <li className="m-8 font-satoshi tracking-widest duration-200 transition"
+          onMouseEnter={changeTitle}>
             <Text customClass="text-dark-300">
               <Link to="/">Home</Link>
             </Text>
           </li>
-          <li className="m-8  font-satoshi tracking-widest  duration-200 transition">
+          <li className="m-8  font-satoshi tracking-widest  duration-200 transition" 
+          onMouseEnter={changeTitle}>
             <Text customClass="text-dark-300">
               <Link to="/about">About</Link>
             </Text>
           </li>
-          <li className="m-8  font-satoshi tracking-widest duration-200 transition">
+          <li className="m-8  font-satoshi tracking-widest duration-200 transition"
+          onMouseEnter={changeTitle}>
             <Text customClass="text-dark-300">
               <Link to="/project">Project</Link>
             </Text>
           </li>
-          <li className="m-8  font-satoshi tracking-widest duration-200 transition">
+          <li className="m-8  font-satoshi tracking-widest duration-200 transition"
+          onMouseEnter={changeTitle}>
             <Text customClass="text-dark-300">
               <Link to="/contact">Contact us</Link>
             </Text>
@@ -43,7 +53,7 @@ function Nav() {
       <Container full align="top" custom="pt-20">
         <Sphere customClass="container-flex text-center">
           <Header headerSize="lg" customClass="tracking-[5rem]">
-            ABOUT
+            {title}
           </Header>
         </Sphere>
       </Container>
