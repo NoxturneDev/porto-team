b22cea21bb23cbe15c408260da510e8bafc2fef6
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-12 12:44:00 +0700

    new nav design prototype

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,87 +1,53 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import gsap from "gsap";
+import { Container, Wrapper } from "./Container";
 import Text from "./Text";
-import { Wrapper } from "./Container";
+import Header from "./Header";
+import Sphere from "./Sphere";
+import { Toggle } from "./ui/Icon";
 
 function Nav() {
-  const [scrolled, setScrolled] = useState(false)
-  const nav = useRef(null)
-
-
-  const scrollDetect = () => {
-    if (window.scrollY > nav.current.offsetHeight + 100) {
-      setScrolled(true)
-      return
-    } else {
-      setScrolled(false)
-      return
-    }
-  }
-
-
-  const animateOnScroll = (condition) => {
-    gsap.set(nav.current, {
-      position: 'static',
-      y: 0
-    })
-
-    if (condition) {
-      gsap.fromTo(nav.current, {
-        opacity: 0,
-        y: -50,
-      }, {
-        opacity: 1,
-        y: 0,
-        position: 'fixed',
-        x:0
-      })
-    }
-  }
-
-  useEffect(() => {
-    animateOnScroll(scrolled)
-    window.addEventListener('scroll', scrollDetect)
-
-    return () => {
-      window.removeEventListener('scroll', scrollDetect)
-    }
-  })
-
-
-
   return (
-    <div ref={nav} className="bg-astro-dark-100 z-20 mb-10">
-      <Wrapper custom={`${scrolled ? 'right-0 top-0 left-0 shadow-md' : ''} flex w-max-screen h-16 px-6 py-5 text-size`}>
-        <div className="flex w-screen  justify-center items-center px-14 ">
-          <div className=" grow p-4 text-white">
-            <Text textSize="md" customClass="header">HELLOWEB</Text>
-          </div>
-          <ul className="container-flex h-14 text-slate-300 ">
-            <li className="m-8">
-              <Text textSize="sm" customClass="tracking-widest ">
-                <Link to="/">Home</Link>
-              </Text>
-            </li>
-            <li className="m-8">
-              <Text textSize="sm" customClass="tracking-widest ">
-                <Link to="/project">Project</Link>
-              </Text>
-            </li>
-            <li className="m-8">
-              <Text textSize="sm" customClass="tracking-widest ">
-                <Link to="/about">About</Link>
-              </Text>
-            </li>
-            <li className="m-8  font-satoshi tracking-widest hover:text-white duration-200 transition">
-              <Text textSize="sm" customClass="tracking-widest ">
-                Contact us
-              </Text>
-            </li>
-          </ul>
-        </div>
+    <div style={{
+      background: 'radial-gradient(circle, #DB0F97, #C20C86)'
+    }}>
+      <Wrapper custom="px-10">
+        <Toggle w="22px" h="22px" />
+        <ul className="flex justify-center items-center color-white h-14 font-semibold text-dark-300 uppercase grow">
+          <li className="m-8 font-satoshi tracking-widest duration-200 transition">
+            <Text customClass="text-dark-300">
+              <Link to="/">Home</Link>
+            </Text>
+          </li>
+          <li className="m-8  font-satoshi tracking-widest  duration-200 transition">
+            <Text customClass="text-dark-300">
+              <Link to="/about">About</Link>
+            </Text>
+          </li>
+          <li className="m-8  font-satoshi tracking-widest duration-200 transition">
+            <Text customClass="text-dark-300">
+              <Link to="/project">Project</Link>
+            </Text>
+          </li>
+          <li className="m-8  font-satoshi tracking-widest duration-200 transition">
+            <Text customClass="text-dark-300">
+              <Link to="/contact">Contact us</Link>
+            </Text>
+          </li>
+        </ul>
+        <Header headerSize="sm" customClass="text-dark-300">
+          ABOUT
+        </Header>
       </Wrapper>
-    </div>
+      <Container full align="top" custom="pt-20">
+        <Sphere customClass="container-flex text-center">
+          <Header headerSize="lg" customClass="tracking-[5rem]">
+            ABOUT
+          </Header>
+        </Sphere>
+      </Container>
+    </div >
   );
 }
 
