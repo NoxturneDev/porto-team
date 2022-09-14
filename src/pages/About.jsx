93c351ae800bfe36e5bfbc80e511b028715edd7f
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-15 00:25:35 +0700

    reset all pages to blank page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,42 +1,10 @@
 import React from "react";
-import { Container } from "../components/Container";
-import Nav from "../components/Nav";
-// import ParticleAnimation from "../animation/ParticleAnimation";
-import Paragraph from "../components/Paragraph";
-import Carousel from "../components/caraousel/Caraousel";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+
 
 function About() {
   return (
     <div style={{ overflowX: "hidden" }} className="relative">
-      <Nav />
-      {/* <div className='flex bg-gray-900 items-center w-screen h-screen'>
-          <h3 className='' >About Page</h3>
-          <Container custom='bg-gray-900' >
-          </Container>
-        </div> */}
-      <Container custom="bg-astro-purple-300 w-full h-[200vh]" dir="column" full particle>
-        {/* <ParticleAnimation /> */}
-        <Header headerSize="lg" customClass="mb-5">
-          ABOUT US
-        </Header>
-
-        <div className="bg-astro-dark-100 mt-7 mb-28   w-4/6 h-2/5"></div>
-        <Paragraph
-          custom="text-zinc-200 mt-28 pt-36 text-size"
-          align="center"
-          animation="fade-up"
-        >
-          Hi! HalloWeb is Digital Service Agency<br /> We can Handle all of your Problem and request for building
-          an Web Design, Web Aplication, and UI UX Design, in HalloWeb we offer the best experience and service<br>
-          </br>
-          With fastest work and Responsibilites About the Project, So Let's Order to HALLOWEB!
-        </Paragraph>
-      </Container>
-
-      <Carousel />
-      <Footer />
+    ABOUT 
     </div>
   );
 }
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,11 +1,4 @@
 import React from "react";
-import Nav from "../components/Nav";
-import {
-  ProjectSectionLeft,
-  ProjectSectionRight,
-} from "../components/project/ProjectSection";
-import Caraousel from "../components/caraousel/Caraousel";
-import Footer from "../components/Footer";
 
 function Project() {
   const desc = {
@@ -17,37 +10,8 @@ function Project() {
     five: "AntarMedika merambah bisnis Teknologi informasi dibidang palayanan jasa kesehatan, mulai dari aplikasi manajemen,perangkat,fasilitas kesehatan,konsultan, dan SDM.",
   };
   return (
-    <div style={{ overflowX: "hidden" }} className=" bg-astro-purple-300 relative">
-      <Nav />
-      <div className="font-bold text-center container-flex-col-b mt-20 h-[20vh]  text-yellow-300">
-        <h1 className="tracking-[.25em] header-lg">OUR PROJECTS</h1>
-        <div className=" bg-astro-purple-100 w-[28em] h-[5px] flex  inset-x-20 rounded-full">
-          {/* <div className="border border-red-500"></div> */}
-        </div>
-      </div>
-      <ProjectSectionLeft
-        img="../assets/image/antarmedika.png"
-        title="ANTAR MEDIKA LANDING PAGE"
-        desc={desc.one}
-        url="https://antarmedika.com/"
-        animation
-      />
-      <ProjectSectionRight
-        img="../assets/image/23.png"
-        title="ANTAR MEDIKA LANDING PAGE"
-        desc={desc.two}
-        url="https://antarmedika.com/"
-        animation
-      />
-      <ProjectSectionLeft
-        img="../assets/image/23.png"
-        title="ANTAR MEDIKA LANDING PAGE"
-        desc={desc.three}
-        url="https://antarmedika.com/"
-        animation
-      />
-      <Caraousel />
-      <Footer />
+    <div style={{ overflowX: "hidden" }} className=" bg-dark-300 relative">
+      
     </div>
   );
 }
