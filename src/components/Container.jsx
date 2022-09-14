e53d84c48cd62005bea7d5a420a3d40abc7e6998
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-15 00:27:23 +0700

    refactor components Container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,95 +5,59 @@ import React from 'react'
  * @param {String} children - components children
  * @param {String} custom - custom tailwind classes for the components
  * @param {Boolean} full - default : false, fullscreen size
- * @param {String} dir - default : row, values = [top, bot] 
+ * @param {String} direction - default : row, values = [top, bot] 
  * @param {String} align - default : center, values [center, left, right, top, bot]
  * @returns react container components
  */
 
+const rowContainer = {
+    center: 'container-flex',
+    right: 'container-flex-r',
+    left: 'container-flex-l',
+    top: 'container-flex items-start',
+    bot: 'container-flex items-end'
+}
 
-export function Container({ children, custom = '', full = false, align = 'center', dir = 'row' }) {
+const colContainer = {
+    center: 'container-flex-col',
+    right: 'container-flex-col items-end',
+    left: 'container-flex-col items-start',
+    top: 'container-flex-col-t',
+    bot: 'container-flex-col-b'
+}
 
-    const rowContainer = {
-        center: 'container-flex',
-        right: 'container-flex-r',
-        left: 'container-flex-l',
-        top: 'container-flex items-start',
-        bot: 'container-flex items-end'
-    }
-
-    const colContainer = {
-        center: 'container-flex-col',
-        right: 'container-flex-col items-end',
-        left: 'container-flex-col items-start',
-        top: 'container-flex-col-t',
-        bot: 'container-flex-col-b'
-    }
-
-    function handleType(dir) {
-        switch (dir) {
-            case 'row': {
-                for (const al in rowContainer) {
-                    if (align === al) {
-                        return rowContainer[al]
-                    }
+function handleType(align, direction) {
+    switch (direction) {
+        case 'row': {
+            for (const al in rowContainer) {
+                if (align === al) {
+                    return rowContainer[al]
                 }
             }
-            case 'column': {
-                for (const al in colContainer) {
-                    if (align === al) {
-                        return colContainer[al]
-                    }
+        }
+        case 'column': {
+            for (const al in colContainer) {
+                if (align === al) {
+                    return colContainer[al]
                 }
             }
         }
     }
+}
 
+
+export function Container({ children, customClass = '', full = false, align = 'center', direction = 'row' }) {
     return (
-        <div className={`px-4 py-8 ${handleType(dir)} ${full ? 'h-[125vh] w-screen' : 'h-full w-full'} ${custom}`}>
+        <div className={`px-4 py-8 ${handleType(align, direction)} ${full ? 'h-[125vh] w-screen' : 'h-full w-full'} ${customClass}`}>
             {children}
         </div>
     )
 }
 
-export function Wrapper({ children, custom = '', align = 'center', dir = 'row' }) {
-
-    const rowContainer = {
-        center: 'container-flex',
-        right: 'container-flex-r',
-        left: 'container-flex-l',
-        top: 'container-flex items-start',
-        bot: 'container-flex items-end'
-    }
-
-    const colContainer = {
-        center: 'container-flex-col',
-        right: 'container-flex-col items-end',
-        left: 'container-flex-col items-start',
-        top: 'container-flex-col-t',
-        bot: 'container-flex-col-b'
-    }
-
-    function handleType(dir) {
-        switch (dir) {
-            case 'row': {
-                for (const al in rowContainer) {
-                    if (align === al) {
-                        return rowContainer[al]
-                    }
-                }
-            }
-            case 'column': {
-                for (const al in colContainer) {
-                    if (align === al) {
-                        return colContainer[al]
-                    }
-                }
-            }
-        }
-    }
-
+// WITHOUT PADDING, WIDTH AND HEIGHT ADJUST TO IT'S CHILDREN
+export function Wrapper({ children, customClass = '', align = 'center', direction = 'row' }) {
     return (
-        <div className={`h-full w-full ${handleType(dir)} ${custom}`}>
+        <div className={`h-max w-max ${handleType(align, direction)} ${customClass}`}>
             {children}
         </div>
     )
