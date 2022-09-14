b8d0f7b78bb21e2058a1d6fa0bf556b628a16aa8
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-15 00:36:38 +0700

    reset footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,7 @@ import Paragraph from "./Paragraph";
 const Footer = () => {
     return (
         <>
-            <Wrapper custom='h-28 bg-astro-dark-100' align='center' >
-                <Paragraph align='center' custom='text-white tracking-wider' size="sm">
-                    Lorem Lorem ipsum dolor sit amet consectetur adipiscing elit,
-                </Paragraph>
-            </Wrapper>
+            <h1>Footer</h1>
         </>
     )
 }
