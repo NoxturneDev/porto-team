c08d3defb7c77aba0f8d1b4c0664676ce6601a55
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-15 00:33:33 +0700

    fix sphere color and animaiton

--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef} from 'react'
 import gsap from 'gsap'
 import { Container } from './Container'
-import { useRef } from 'react'
 
-function Sphere() {
+function Sphere({ children, customClass }) {
     const [deg, setDeg] = useState(70)
     const sphere = useRef(null)
 
     const changeDegree = () => {
-
-        
         const scroll = window.scrollY
-        
-        // if (scroll % 100 === 0 && scroll !== 0) {
-        //     setDeg(120)
-        //     return
-        // }
+
         setDeg(scroll)
-        // gsa.fromTo(sphere.current, {
-        //     background:  `linear-gradient(70deg, rgba(40, 40, 40, 1), rgba(22, 22, 22, 1)`
-        // }, {
-        //     background:  `linear-gradient(120deg, rgba(40, 40, 40, 1), rgba(22, 22, 22, 1)`
-        // })
     }
 
     useEffect(() => {
@@ -33,17 +21,15 @@ function Sphere() {
     }, [])
 
     return (
-        <Container full>
-            <div
-                ref={sphere}
-                className="w-72 h-72 rounded-full shadow-md"
-                style={{
-                    background:  `linear-gradient(${deg}deg, rgba(40, 40, 40, 1), rgba(22, 22, 22, 1)`
-                }}
-            >
-                
-            </div>
-        </Container>
+        <div
+            ref={sphere}
+            className={`w-[400px] h-[400px] rounded-full shadow-md ${customClass}`}
+            style={{
+                background: `linear-gradient(${deg}deg, #DB0F97, #600040`
+            }}
+        >
+            {children}
+        </div>
     )
 }
 
