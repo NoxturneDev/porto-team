7d87ad7db08a81630be12ec627026d4082382852
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-09-15 17:59:56 +0700

    smooth scroll prototype

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Section } from '../components/layout/Container'
 import HeaderMarquee from '../animation/HeaderMarquee'
+import luxy from 'luxy.js'
+import { useEffect } from 'react'
 
 function Demo() {
+    const init = () => {
+        luxy.init()
+
+        luxy.settings.wrapperSpeed = 0.01
+    }
+    useEffect(() => {
+        init()
+    }, [])
     return (
-        <div style={{ overflowX: 'hidden'}}>
+        <div style={{ overflowX: 'hidden'}} id="luxy">
             <Section customClass="bg-radial-main">
                 <HeaderMarquee>TEST-MARQUEE</HeaderMarquee>
             </Section>
