ae8d065c035ca5591eac2717bdd9cf99f6ea3c6c
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-10-27 02:47:57 +0700

    finished about detail title animation on click and about page design

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Section } from '../../components/layout/Container'
+import { Wrapper } from '../../components/layout/Container'
 import AboutDetail from "./AboutDetail";
+import Line from "../../components/Line"
+import Nav from "../../components/Nav"
 
 function About() {
   return (
-    <div style={{ overflowX: "hidden" }} className="relative">
-      <Section customClass="bg-radial-dark" direction="column" align="left">
+    <div style={{ overflowX: "hidden" }} className="relative bg-radial-dark">
+      <Nav />
+      <Line>ABOUT</Line>
+      <Wrapper customClass="w-full min-h-screen max-h-max bg-radial-dark pt-10" direction="column" align="left">
         <AboutDetail title="HALLOWEB IS A VERY COOL AGENCY DUDE!">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, molestiae ad asperiores est autem consectetur facilis. Dicta ex autem distinctio.j</AboutDetail>
         <AboutDetail title="HALLOWEB IS A VERY COOL AGENCY DUDE!">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, molestiae ad asperiores est autem consectetur facilis. Dicta ex autem distinctio.j</AboutDetail>
         <AboutDetail title="HALLOWEB IS A VERY COOL AGENCY DUDE!">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, molestiae ad asperiores est autem consectetur facilis. Dicta ex autem distinctio.j</AboutDetail>
-      </Section>
+      </Wrapper>
     </div>
   );
 }
--- src/pages/about/AboutDetail.jsx
+++ src/pages/about/AboutDetail.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import gsap from 'gsap'
+import { Wrapper } from '../../components/layout/Container'
+import Header from '../../components/Header'
+import Text from '../../components/Text'
 
 function AboutDetail({ title, children: text }) {
+    const [active, setActive] = useState('')
+    const disableClick = useRef(false)
 
-    const revealAnimation = (target, opened) => {
+    const revealAnimation = (target, status) => {
         // target[0] as parent, target[1] as child
         const tl = gsap.timeline()
 
-        if (opened) {
+        if (status === "opened") {
+            // close animation 
             tl.fromTo(target[1],
-                { opacity: 1, y: 0, display: 'block', ease: 'power2.out', duration: 1 },
+                { opacity: 1, y: 0, display: 'block', ease: 'power4.out', duration: 1 },
                 { opacity: 0, y: 100, display: 'hidden' }
             )
-            tl.to(target[0], { height: '25vh', backgroundColor: 'transparent' }, '>0.5')
+            tl.to(target[0], { height: '25vh', ease: 'power2.in', duration: 0.8, onComplete: () => disableClick.current = false }, '<')
         } else {
-            tl.to(target[0], { height: '40vh', backgroundColor: '#C20C86' })
+            tl.to(target[0], { height: '40vh', ease: 'power2.in', duration: 0.8 })
             tl.fromTo(target[1],
                 { opacity: 0, y: 100, display: 'hidden' },
-                { opacity: 1, y: 0, display: 'block', ease: 'power2.out', duration: 1 }, '>0.5'
+                { opacity: 1, y: 0, display: 'block', ease: 'power4.out', duration: 1, onComplete: () => disableClick.current = false }, '<0.5'
             )
         }
     }
@@ -25,19 +31,26 @@ function AboutDetail({ title, children: text }) {
     const revealTextOnClick = (e) => {
         e.stopPropagation()
 
+
         const clicked = e.target,
             target = !clicked.dataset.clickable ? clicked.parentElement : clicked, //checking if the selected element isn't the actual element or not
             children = target.children,
             textInChildren = children[1].children
 
         // if the element is clicked 
-        if (target.dataset.opened === "true") {
-            target.dataset.opened = "false"
-            revealAnimation([target, textInChildren], true)
-        } else {
-            revealAnimation([target, textInChildren], false)
-            target.dataset.opened = "true"
-        }
+        if (!disableClick.current)
+            if (target.dataset.opened === "true") {
+                target.dataset.opened = "false"
+                revealAnimation([target, textInChildren], "opened")
+                setActive(currActive => currActive = false)
+            } else {
+                revealAnimation([target, textInChildren], "not opened")
+                target.dataset.opened = "true"
+                setActive(currActive => currActive = true)
+            }
+
+        // wait for the animation to stop until it can get clicked again
+        disableClick.current = true
     }
 
     return (
@@ -45,15 +58,15 @@ function AboutDetail({ title, children: text }) {
             onClick={e => revealTextOnClick(e)}
             data-clickable="true"
             data-opened="false"
-            className='p-6 w-full hover:bg-main-200 transition-all h-[25vh]'
+            className={`p-6 w-full ${active && active !== "" ? 'bg-main-200' : ''} hover:bg-main-200 transition-all h-[25vh]`}
         >
-            <h1 className='text-[2rem] header text-white mb-4'>{title}</h1>
-            <p className='w-full text-white overflow-hidden'>
-                <span className='w-3/4 hidden font-satoshi font-semibold tracking-wider'>
+            <Header headerSize="md" customClass="container-flex-l">{title}</Header>
+            <Text customClass='w-full text-white overflow-hidden my-4' textSize="md">
+                <span className='w-3/4 hidden tracking-wider'>
                     {text}
                 </span>
-            </p>
-        </div>
+            </Text>
+        </div >
     )
 }
 
